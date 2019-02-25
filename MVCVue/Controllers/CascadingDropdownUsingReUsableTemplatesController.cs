cefd3133ec8bb72b0672ff8f377e9170277ab605
Neil Dobson <ndobson@barnardos.org.au>
2019-02-25 17:22:23 +1100

    Reusable template

---
+++ MVCVue/Controllers/CascadingDropdownUsingReUsableTemplatesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCVue.Models;
+
+namespace MVCVue.Controllers
+{
+    public class CascadingDropdownUsingReUsableTemplatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new CarModel { BrandId = CarBrands.Kia, ModelId = CarModels.KiaSoul });
+        }
+
+        [HttpPost]
+        public IActionResult Index(CarModel viewModel)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
